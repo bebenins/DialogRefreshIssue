0323c2abebbe074bb33d62f65ddcfb6d0e108012
bebenins <s_bebenin@yahoo.com>
2018-10-29 15:54:03 -0400

    Adding project

---
+++ DialogRefreshIssue/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+using MahApps.Metro.Controls;
+
+namespace DialogRefreshIssue
+{
+    public partial class MainWindow : MetroWindow, INotifyPropertyChanged
+    {
+        private bool m_isInitializing;
+        public bool IsInitializing
+        {
+            get { return m_isInitializing; }
+            set { m_isInitializing = value; OnPropertyChanged(); }
+        }
+
+        private object m_progressInitialization;
+        public object ProgressInitialization
+        {
+            get { return m_progressInitialization; }
+            set
+            {
+                if (m_progressInitialization == value) return;
+                m_progressInitialization = value; OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+            ProgressInitialization = new ProgressDialog("Initializing...");
+        }
+
+        private void ButtonTest_Click(object sender, RoutedEventArgs e)
+        {
+            IsInitializing = !IsInitializing;
+        }
+    }
+}
---
+++ DialogRefreshIssue/ProgressDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace DialogRefreshIssue
+{
+    public partial class ProgressDialog : UserControl
+    {
+        private string m_progressTitle;
+        public string ProgressTitle
+        {
+            get { return m_progressTitle; }
+            set { m_progressTitle = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ProgressDialog(string title)
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            ProgressTitle = title;
+        }
+    }
+}
